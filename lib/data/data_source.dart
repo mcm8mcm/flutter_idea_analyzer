a6285466f1ef9ec1a914efec9834faaae9614dfd
MCM <mcmfspm@gmail.com>
2020-10-12 18:51:56 +0300

    add classificator list

---
+++ lib/data/data_source.dart
@@ -0,0 +1,56 @@
+class Ideaclassificator {
+  final List<Map<String, dynamic>> _ideaClassificatorList = [
+    {
+      'number': 1,
+      'caption': '',
+      'description': '',
+    },
+    {
+      'number': 2,
+      'caption': '',
+      'description': '',
+    },
+    {
+      'number': 3,
+      'caption': '',
+      'description': '',
+    },
+    {
+      'number': 4,
+      'caption': '',
+      'description': '',
+    },
+    {
+      'number': 5,
+      'caption': '',
+      'description': '',
+    },
+    {
+      'number': 6,
+      'caption': '',
+      'description': '',
+    },
+    {
+      'number': 7,
+      'caption': '',
+      'description': '',
+    },
+    {
+      'number': 8,
+      'caption': '',
+      'description': '',
+    },
+    {
+      'number': 9,
+      'caption': '',
+      'description': '',
+    },
+    {
+      'number': 10,
+      'caption': '',
+      'description': '',
+    },
+  ];
+
+  List<Map<String, dynamic>> get classificator => _ideaClassificatorList;
+}
